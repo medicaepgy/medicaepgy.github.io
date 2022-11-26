04d94dd778da7d8d39243f00075efa8e439447e9
medicaepgy <119121831+medicaepgy@users.noreply.github.com>
2022-11-26 11:19:08 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,1 +1,14 @@
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<p>
+        &copy; 2022
+        Ricardo Armando Machorro Reyes.
+      </p>`;
+  }
+}
+
+customElements.define(
+  "mi-footer", MiFooter);
 
